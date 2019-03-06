396844f0d65b69eb196ba528ca7f5934ad53f297
Maggie Marino <mmarino4@nd.edu>
2019-03-06 13:11:33 -0500

    lowercase i

--- popup.js
+++ popup.js
@@ -3,15 +3,8 @@
 // Button to open a modal
 $(document).ready(function (){
     //ID or Class name of the button goes into the first section
-    $('#place-button-name-here').click(function() {
+    $(".actual-alabama").click(function() {
         //ID or Class name of the modal you want to appear on click goes into this second area.
-        $("#place-modal-name-here").toggleClass("visible");
-        });
-});
-
-// Button to close a modal
-$(document).ready(function (){
-    $('#place-modal_close-icon-name-here').click(function() {
-        $("#place-modal-name-here").toggleClass("visible")
+        $(".reveal-school").toggleClass("visible");
         });
 });

8b7d11c233034645993a484a6fd135fc9ea2f7f2
Maggie Marino <mmarino4@nd.edu>
2019-04-03 05:25:10 -0400

    uppity update

--- court-cases.js
+++ court-cases.js
@@ -4,3 +4,33 @@ $(document).ready(function (){
         $(".board-of-regents-reveal").toggleClass("visible");
         });
 });
+$(document).ready(function (){
+
+    $(".tarkanian").click(function() {
+        $(".tarkanian-reveal").toggleClass("visible");
+        });
+});
+$(document).ready(function (){
+
+    $(".law").click(function() {
+        $(".law-reveal").toggleClass("visible");
+        });
+});
+$(document).ready(function (){
+
+    $(".white").click(function() {
+        $(".white-reveal").toggleClass("visible");
+        });
+});
+$(document).ready(function (){
+
+    $(".obannon").click(function() {
+        $(".obannon-reveal").toggleClass("visible");
+        });
+});
+$(document).ready(function (){
+
+    $(".alston").click(function() {
+        $(".alston-reveal").toggleClass("visible");
+        });
+});
